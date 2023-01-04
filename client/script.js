551966c333fc9e1517cbfc0ac1fd6c498a4a9fef
mrechavez <milesechavez@gmail.com>
2023-01-04 07:03:52 -0500

    updated server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
 
-  const response = await fetch('https://alfred-ge7t.onrender.com', {
+  const response = await fetch('https://alfred-ge7t.onrender.com/', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
